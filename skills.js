c86915c00490abd06178a49ada58a4450f0fbb7a
Giovanni Mikhael Gonzalez Bergamo <69176031+gMBergamo@users.noreply.github.com>
2024-05-23 18:06:13 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,12 @@
+function CaculateNumbers(var1, var2)
+{
+    return var1 + var2;
+} 
+
+function SubtractNumbers(var1, var2){
+    return var1 - var2;
+}
+
+function divideNumbers(var1, var2) {
+    
+}
